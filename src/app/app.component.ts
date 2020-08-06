34289a952b0a2edc1405b866002d86ccb3a4500e
UasaLine <yury.salagaev@gmail.com>
2020-08-06 09:13:44 +0700

    add app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OAuthService} from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Oauth-app-test';
+
+  constructor(private oauthService: OAuthService) {
+
+  }
+
+  private async configAuth(): Promise<void> {
+    this.oauthService.issuer = 'https://login.microsoftonline.com/common/oauth2/authorize';
+    this.oauthService.loginUrl = 'https://login.microsoftonline.com/common/oauth2/authorize';
+    this.oauthService.clientId = 'c8544830-cc26-4ba5-8f2b-ce4fed11e7e2';
+    this.oauthService.resource = 'api://1f8204a9-1cf9-4007-9f25-8abe9ff4d902';
+    this.oauthService.logoutUrl = 'https://www.npmjs.com/package/angular-oauth2-oidc';
+    this.oauthService.redirectUri = 'http://localhost:4200/';
+    this.oauthService.scope = 'email';
+    this.oauthService.oidc = false;
+    this.oauthService.setStorage(sessionStorage);
+    this.oauthService.responseType = 'code';
+  }
+
+  async ngOnInit() {
+    this.configAuth();
+    await this.oauthService.initImplicitFlow();
+    await this.oauthService.tryLogin({});
+    alert(this.oauthService.getIdentityClaims());
+
+    alert(this.oauthService.getAccessToken());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OAuthModule, OAuthService} from 'angular-oauth2-oidc';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost'],
+        sendAccessToken: true
+      }
+    }),
   ],
-  providers: [],
+  providers: [
+    OAuthService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
